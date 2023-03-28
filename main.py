e50861c9f53c7c9d7029c90a172a33ebc8fc4b4f
ss3z <129182743+ss3z@users.noreply.github.com>
2023-03-28 23:15:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageGrab
+import time
+
+# делаем задержку в 2 секунды на выбор
+# окна, для которого нужно сделать скриншот
+time.sleep(2)
+
+# создание скриншота
+img = ImageGrab.grab()
+# сохраним скриншот
+img.save("test2.jpg", quality="web_medium")
+# откроем созданный скриншот
+with Image.open("test2.jpg") as img:
+    # смотрим, какую информацию
+    # пишет библиотека Pillow
+    # при создании скриншота
+    print(img.info)
+img.save("image_name.jpg", quality="web_high")
